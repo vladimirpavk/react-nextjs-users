029d61d142f2508a31a8308f4afc90fd5c1e6f8c
Vladimir Pavković <vladimirpavk@gmail.com>
2021-09-10 11:55:08 +0200

    +

--- src/App.js
+++ src/App.js
@@ -8,13 +8,20 @@ function App() {
 
   const addNewUser = (eventData)=>{
     //console.log(eventData);
-    setUsersList((oldValue)=>[...oldValue, eventData]);
+    setUsersList((oldValue)=>[...oldValue, {id:Math.random(), ...eventData}]);
+  }
+
+  const removeUser = (idUser)=>{
+    //console.log('remove user', idUser);
+    setUsersList(
+      oldUsers=>oldUsers.filter(element=>element.id!==idUser)
+    )
   }
 
   return (
     <div>
       <AddUser userAdded={addNewUser}></AddUser>
-      <UserList userlist={}></UserList>
+      <UserList userlist={usersList} deleteUser={removeUser}></UserList>
     </div>
   );
 }
--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,5 +1,21 @@
 import styles from './UserList.module.css';
 
 const UserList = (props)=>{
+    
+    const userClicked = (idUser)=>{
+        //console.log(props);
+        props.deleteUser(idUser);
+    }
 
+    return(
+        <div className={styles.users}>
+            <ul>
+                {
+                    props.userlist.map(element=><li key={element.id} onClick={eventData=>userClicked(element.id)}>{element.id} - {element.username} - {element.age}</li>)
+                }
+            </ul>
+        </div>
+    )
 }
+
+export default UserList;

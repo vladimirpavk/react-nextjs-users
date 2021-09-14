053c808d19f44ff08405e44107472d3b6e59a08a
Vladimir Pavković <vladimirpavk@gmail.com>
2021-09-14 10:28:15 +0200

    +

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 import AddUser from './components/AddUser/AddUser';
 import UserList from './components/UserList/UserList'
 
+import MainHeader from './components/MainHeader/MainHeader';
 import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+ /*  const [usersList, setUsersList] = useState([]);
 
   const addNewUser = (eventData)=>{
     //console.log(eventData);
@@ -18,13 +20,26 @@ function App() {
     setUsersList(
       oldUsers=>oldUsers.filter(element=>element.id!==idUser)
     )
+  } */
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (username, password)=>{
+    console.log(username, password);
+    setIsLoggedIn(true);
+  }
+
+  const logoutHandler = ()=>{
+    setIsLoggedIn(false);
   }
 
   return (
-    <div>
+    <div className="main">
      {/*  <AddUser userAdded={addNewUser}></AddUser>
       <UserList userlist={usersList} deleteUser={removeUser}></UserList> */}
-      <Login></Login>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={(eventData)=>logoutHandler(eventData)}></MainHeader>
+      {!isLoggedIn && <Login onLogin={loginHandler}></Login>}
+      {isLoggedIn && <Home onLogout={(eventData)=>logoutHandler(eventData)}></Home>}
     </div>
   );
 }
